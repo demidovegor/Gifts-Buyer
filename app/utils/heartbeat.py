6f41c09df69f46e804d32801a00468aefd2bc8bb
demidov <demaxpro@bk.ru>
2025-06-16 03:14:53 +0300

    fix async in heartbeat.py

--- app/utils/heartbeat.py
+++ app/utils/heartbeat.py
@@ -2,7 +2,7 @@ import urllib.request
 from app.utils.logger import error
 from data.config import config
 
-async def send_heartbeat():
+def send_heartbeat():
     try:
         urllib.request.urlopen(config.HEARTBEAT_MONITOR_URL, timeout=2)
     except Exception as e:
